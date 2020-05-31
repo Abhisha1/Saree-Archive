8a0dab141f45fd9dbaa16e8d0f6ec20f3609649c
Abhisha Nirmalathas <a.nirmalathas1@student.unimelb.edu.au>
2020-06-01 09:51:14 +1000

    checking routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from "./pages/Landing";
+import Login from "./pages/Login";
 import PrivateRoute from "./components/protectedRoutes";
 import NavBar from "./components/Navbar";
-import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Add from "./pages/Add";
 import './App.css';
@@ -15,12 +15,12 @@ function App() {
       <div className="App">
         <NavBar />
         <Route path="/" exact component={LandingPage} />
-        <PrivateRoute path="/home" component={Home} exact={true} />
-        <PrivateRoute path="/add" component={Add} exact={true} />
+        <Route path="/login" exact component={Login}/>
+        {/* {/* <PrivateRoute path="/home" component={Home} exact={true} /> */}
+        <PrivateRoute path="/add" component={Add} exact={true} /> 
         {/* <PrivateRoute path="/view" component={Home} exact={true} />
         <PrivateRoute path="/discover" component={Home} exact={true} />
         <Route path="/user" component={Home} exact={true} /> */}
-        <Route path="/login" exact component={Login}/>
        </div>
     </Router>
   );
