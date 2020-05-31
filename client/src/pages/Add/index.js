0b0b5fa11d04534114aa1965a5744f87ff786631
Abhisha Nirmalathas <a.nirmalathas1@student.unimelb.edu.au>
2020-05-31 20:40:12 +1000

    check other components

--- client/src/pages/Add/index.js
+++ client/src/pages/Add/index.js
@@ -6,6 +6,36 @@ import "./add.scss";
 function Add() {
     return (
         <form>
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img src="..." class="d-block w-100" alt="..." />
+                    </div>
+                    <div class="carousel-item">
+                    <img src="..." class="d-block w-100" alt="..." />
+                    </div>
+                    <div class="carousel-item">
+                    <img src="..." class="d-block w-100" alt="..." />
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+                </div>
+            <div class="custom-file">
+                <input type="file" class="custom-file-input" id="customFile" />
+                <label class="custom-file-label" for="customFile">Choose file</label>
+            </div>
             <div class="form-group">
                 <label for="exampleFormControlSelect1">Location</label>
                 <select class="form-control" id="exampleFormControlSelect1">
@@ -32,8 +62,8 @@ function Add() {
             </div>
             <div class="form-group">
                 <label for="exampleFormControlTextarea1">Last worn</label>
-                
             </div>
+
         </form>
     )
 }
