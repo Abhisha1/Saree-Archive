497cf46da4b8e0bec42fe1d0f1598e4e4c7d071f
Abhisha Nirmalathas <a.nirmalathas1@student.unimelb.edu.au>
2020-07-02 20:15:55 +1000

    getting read of bug

--- backend/routes/sarees.js
+++ backend/routes/sarees.js
@@ -7,20 +7,4 @@ router.route('/').get((req, res) => {
         .then(users => res.json(users))
         .catch(err => res.status(400).json('Error: ' + err));
 });
-router.route('/add').post((req, res) => {
-    let newItem = new Item();
-    const newSaree = new Saree({
-        owner: req.body.userId,
-        imgs: [ {data: fs.readFileSync(req.files.userPhoto.path),
-            contentType = 'image/png'
-        }],
-        location: req.body.location
-    });
-    newSaree.save()
-        .then(() => {
-            res.status(200).json({msg: "User added!", id: newUser._id});
-        })
-        .catch(err => res.status(400).json('Error: ' + err));
-});
-
 module.exports = router;
